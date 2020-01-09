4eef0cd2c797d6264427cbfe7a3777a5ca2f6aa1
Hasi6 <37216970+Hasi6@users.noreply.github.com>
2020-01-09 16:51:52 +0530

    first commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>Flask React Chat App</div>;
+};
+
+export default App;
---
+++ client/src/config/index.js
@@ -0,0 +1,5 @@
+export const endPoint = "http://192.168.8.100:5000";
+export const config = {
+  "Content-Type": "application/json"
+};
+// 172.16.41.112
